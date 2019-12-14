2a23777264364b661d5c3871d039dc888cd50350
Julian Trupe <juliantrupe@gmail.com>
2019-12-14 11:18:28 -0800

    new branch push

---
+++ pages/data/waitlistData.js
@@ -0,0 +1,10 @@
+var waitlistArr = [
+    {
+        partyName: "Sands",
+        partyEmail: "sandofabeach@email.com",
+        partyID: "32",
+        phoneNumber: '999-999-9999'
+    }
+]
+
+module.exports(waitlistArr);
